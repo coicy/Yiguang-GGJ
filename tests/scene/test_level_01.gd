6fc2e3133e11396149033bc4b3446eae519e9c67
coicy <1491094821@qq.com>
2026-09-06 04:15:26 +0800

    test: align level camera assertions

--- tests/scene/test_level_01.gd
+++ tests/scene/test_level_01.gd
@@ -49,7 +49,7 @@ func _run() -> void:
 	assert(not collision.disabled)
 
 	var bounds := level.get_node("CameraBounds") as CameraBounds
-	assert(bounds.get_world_rect().is_equal_approx(Rect2(-256.0, -128.0, 1344.0, 768.0)))
+	assert(bounds.get_world_rect().is_equal_approx(Rect2(-414.0, 190.0, 1500.0, 500.0)))
 	var spawn_icon := level.get_node("SpawnPoint/BrokenTankIcon") as Sprite2D
 	assert(spawn_icon != null, "The current spawn point must display the broken tank icon.")
 	assert(spawn_icon.texture != null)
@@ -83,12 +83,15 @@ func _run() -> void:
 	assert(flower_light.energy > 1.0)
 	assert(flower_light.shadow_enabled)
 	var camera := player.get_node("Camera2D") as Camera2D
-	assert(camera.zoom.is_equal_approx(Vector2(3.0, 3.0)))
 	assert(camera.limit_smoothed)
-	assert(camera.position_smoothing_enabled)
-	assert(is_equal_approx(camera.position_smoothing_speed, 7.0))
-	assert(camera.limit_left == -256 and camera.limit_top == -128)
-	assert(camera.limit_right == 1088 and camera.limit_bottom == 640)
+	assert(camera.limit_left == -414 and camera.limit_top == 190)
+	assert(camera.limit_right == 1086 and camera.limit_bottom == 690)
+	var phantom := level.get_node("PhantomCamera2D") as PhantomCamera2D
+	assert(phantom != null)
+	assert(phantom.follow_mode == PhantomCamera2D.FollowMode.FRAMED)
+	assert(phantom.zoom.is_equal_approx(Vector2(3.0, 3.0)))
+	assert(is_equal_approx(phantom.dead_zone_width, 0.6))
+	assert(is_equal_approx(phantom.dead_zone_height, 0.52))
 
 	level.queue_free()
 	quit()
